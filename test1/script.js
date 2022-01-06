b7a1100b80ce379f94578844d494c7912cb3ef60
stastsyba <stascyba1@gmail.com>
2022-01-06 11:34:39 +0200

    My test work

--- test1/script.js
+++ test1/script.js
@@ -1,4 +1,5 @@
-
+const BASE_URL = 'https://jsonplaceholder.typicode.com';
+let btnAdd = document.querySelector('.form__btn-add').onclick;
 document.querySelector('.form__btn-sub').onclick = myClick;
 document.querySelector('.form__btn-clr').onclick = clearclick;
 let user = {};
@@ -16,12 +17,14 @@ function myClick() {
       document.querySelector('.form__spn2').textContent ='Please complete this step to continue registration';
       document.querySelector('.form__spn3').textContent ='Please complete this step to continue registration';
     } else {
+        getUsers();
+
         user.name = inp1;
         user.surname = inp2;
         user.data = inp3;
 
         for(key in user){
-            let div = document.createElement('div');
+            let div = document.createElement('span');
             div.className = 'user_list';
             div.innerHTML = user[key];
             document.querySelector('.form__inp').appendChild(div); 
@@ -36,3 +39,22 @@ function clearclick() {
     let inp2 = document.querySelector('.i2').value = '';
     let inp3 = document.querySelector('.i3').value = '';
 }
+
+    elem.onclick = async function getUsers(){
+    let resp = await fetch (`${BASE_URL}/users`, {
+        method: 'get',
+    });
+    let users = await resp.json();
+    if(resp.ok) {
+        for(let key in users){
+            res = users[key];
+            let div = document.createElement('span');
+            div.className = 'user_list';
+            div.innerHTML = res;
+            document.querySelector('.form__inp').appendChild(div); 
+        }
+        json.stringify(res);
+    }
+}
+
+
